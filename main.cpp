52d29906162b1b9355d001db0b3cdf58c8f7fb9b
Cortesja <146901338+Cortesja@users.noreply.github.com>
2024-05-28 18:18:33 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <string>
+#include <iostream>
+#include <list>
+#include <vector>
+using namespace std;
 
 int main() {
 
-	return 0;
+	//リストを宣言
+#pragma region
+
+	std::list<const char*> stationName = {
+		"Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno",
+		"Uguisudani", "Nippori", "Tabata", "Komagoe", "Sugamo",
+		"Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba",
+		"Shin-Okubo", "Shinjuku", "Yoyogi", "Harajuku", "Shibuya",
+		"Ebisu", "Meguro", "Gotanda", "Osaki", "Shinagawa",
+		"Tamachi", "Hamamatsucho", "Shimbashi", "Yurakucho"
+	};
+#pragma endregion 
+
+	cout << "1970の山手線の駅" << endl;
+	for (std::list<const char*>::iterator printList = stationName.begin(); printList != stationName.end(); printList++) {
+		cout << *printList << endl;
+	}
+
+
+	cout << endl << "2019の山手線の駅" << endl;
+	for (std::list<const char*>::iterator printList = stationName.begin(); printList != stationName.end(); printList++) {
+		if (*printList == "Tabata") {
+			printList = stationName.insert(printList, "Nishi-Nippori");
+			cout << *printList << endl;
+			++printList;
+		}
+		cout << *printList << endl;
+	}
+
+	cout << endl << "2022の山手線の駅" << endl;
+	for (std::list<const char*>::iterator printList = stationName.begin(); printList != stationName.end(); printList++) {
+		if (*printList == "Tamachi") {
+			printList = stationName.insert(printList, "Takanawa Gateway");
+			cout << *printList << endl;
+			++printList;
+		}
+		cout << *printList << endl;
+	}
 }
