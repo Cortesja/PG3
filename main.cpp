9dc38e2f6a998ec29fac98e2c23d99058105c950
Cortesja <146901338+Cortesja@users.noreply.github.com>
2024-06-07 16:31:36 +0900

    調整２

--- main.cpp
+++ main.cpp
@@ -2,39 +2,39 @@
 #include <iostream>
 using namespace std;
 
-template <typename Type>
+template <typename Type1, typename Type2>
 class Compare {
 public:
-    Compare(Type v1, Type v2) : value1_(v1), value2_(v2) {} //コンストラクター
+    Compare(Type1 v1, Type2 v2) : value1_(v1), value2_(v2) {} //コンストラクター
 
-    Type getMinResult() { //minを出力関数
+    Type1 getMinResult() { //minを出力関数
         if (value1_ > value2_) {
-            return static_cast<Type> (value2_);
+            return static_cast<Type1> (value2_);
         }
-        return static_cast<Type> (value1_);
+        return static_cast<Type1> (value1_);
     }
 private:
-    Type value1_;
-    Type value2_;
+    Type1 value1_;
+    Type2 value2_;
 };
 
 
 int main() {
-    Compare<int> compareInt(1, 2); //Type value1_とType value2_に1と2の値でインスタンスcompareIntを制作する
-    Compare<float> compareFloat(24.5f, 1.0f);
-    Compare<double> compareDouble(23.111, 1.0);
+    Compare<int, int> compareInt(1, 2); //Type value1_とType value2_に1と2の値でインスタンスcompareIntを制作する
+    Compare<float, float> compareFloat(24.5f, 1.0f);
+    Compare<double, double> compareDouble(23.111, 1.0);
 
-    Compare<int> compareIntDouble(14.555, 1);
-    Compare<double> compareDoubleInt(1.000, 33.2233);
-    Compare<float> compareFloatDouble(1.0f, 3444.4);
+    Compare<double, int> compareIntDouble(14.555, 1);
+    Compare<double, int> compareDoubleInt(1, 33.2233);
+    Compare<float, double> compareFloatDouble(1.0f, 3444.4);
 
     //比べた後の結果を出力
-    printf("%d\n", compareInt.getMinResult());
-    printf("%0.2f\n", compareFloat.getMinResult());
-    printf("%lf\n", compareDouble.getMinResult());
+    cout << compareInt.getMinResult() << endl;
+    cout << compareFloat.getMinResult() << endl;
+    cout << compareDouble.getMinResult() << endl;
 
-    printf("%d\n", compareIntDouble.getMinResult());
-    printf("%lf\n", compareDoubleInt.getMinResult());
-    printf("%0.2f\n", compareFloatDouble.getMinResult());
+    cout << compareIntDouble.getMinResult() << endl;
+    cout << compareDoubleInt.getMinResult() << endl;
+    cout << compareFloatDouble.getMinResult() << endl;
 	return 0;
 }
