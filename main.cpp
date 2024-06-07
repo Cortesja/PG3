709d5f3b8fdb3f04a72560d6d5ee11dee4553ef6
Cortesja <146901338+Cortesja@users.noreply.github.com>
2024-06-07 16:02:46 +0900

    コメントを入れました。

--- main.cpp
+++ main.cpp
@@ -5,16 +5,9 @@ using namespace std;
 template <typename Type>
 class Compare {
 public:
-    Compare(Type v1, Type v2) : value1_(v1), value2_(v2) {}
+    Compare(Type v1, Type v2) : value1_(v1), value2_(v2) {} //コンストラクター
 
-    Type getValue1() {
-        return value1_;
-    }
-    Type getValue2() {
-        return value2_;
-    }
-
-    void printMinResult() {
+    void printMinResult() { //minを出力関数
         Type result;
         if (value1_ > value2_) {
             result = static_cast<Type> (value2_);
@@ -24,7 +17,6 @@ public:
         }
         cout << result << endl;
     }
-
 private:
     Type value1_;
     Type value2_;
@@ -32,14 +24,15 @@ private:
 
 
 int main() {
-    Compare<int> compareInt(1, 2);
-    Compare<float> compareFloat(24.5f, 1);
+    Compare<int> compareInt(1, 2); //Type value1_とType value2_に1と2の値でインスタンスcompareIntを制作する
+    Compare<float> compareFloat(24.5f, 1.0f);
     Compare<double> compareDouble(23.111, 1.0);
 
     Compare<int> compareIntDouble(14.555, 1);
-    Compare<double> compareDoubleInt(1.11, 33);
-    Compare<float> compareFloatDouble(23.0f, 3444.4);
+    Compare<double> compareDoubleInt(1.000, 33.2233);
+    Compare<float> compareFloatDouble(1.0f, 3444.4);
 
+    //比べた後の結果を出力
     compareInt.printMinResult();
     compareFloat.printMinResult();
     compareDouble.printMinResult();
