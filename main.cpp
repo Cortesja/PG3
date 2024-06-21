6f72e77f48f6b798014ee81e0244fe7dd4e60f50
Cortesja <146901338+Cortesja@users.noreply.github.com>
2024-06-21 15:10:52 +0900

    やり直し完成

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include "Animal.h"
 #include "Cat.h"
 #include "Dog.h"
 #include "Chicken.h"
 int main() {
-	Animal* cat = new Cat("猫ちゃん");
-	Animal* dog = new Dog("ワンちゃん");
-	Animal* chicken = new Chicken("鶏ちゃん");
+	Cat* cat = new Cat("猫ちゃん");
+	Dog* dog = new Dog("ワンちゃん");
+	Chicken* chicken = new Chicken("鶏ちゃん");
 
 	cat->Cry();
 	dog->Cry();
